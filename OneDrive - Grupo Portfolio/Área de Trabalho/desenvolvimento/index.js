a1c10e8811f39bfca9fcc6586a80a2bed605b16d
Andressa Gomes <andressabgomes@gmail.com>
2025-10-02 13:25:55 -0300

    Corrigir estrutura do projeto: adicionar index.js e configurar Railway corretamente

---
+++ OneDrive - Grupo Portfolio/Área de Trabalho/desenvolvimento/index.js
@@ -0,0 +1,6 @@
+// Arquivo de entrada para Railway
+// Este arquivo força o Railway a reconhecer o projeto como Node.js
+console.log('🚀 Railway detectou projeto Node.js');
+
+// Importar e iniciar o servidor principal
+require('./dist/server.js');
